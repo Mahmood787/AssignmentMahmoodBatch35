2b9773ed72ce476d272565f565374f30419830be
Mahmood787 <ali_khan87@live.com>
2020-06-11 02:25:28 +0500

    second commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(prop) {
   let [count, setcount] =React.useState(0);
   function addd(){
     setcount(count+1);
@@ -10,6 +10,7 @@ function App() {
 
   return (
     <div className="App">
+      This app is created by {prop.name}
       <h1>{count}</h1>
       <button onClick={addd}>add</button>
     </div>
