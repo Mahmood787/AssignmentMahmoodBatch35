578b2ec666bb651776ef4951a58ce654d22ce2da
Mahmood787 <ali_khan87@live.com>
2020-06-11 02:16:37 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let [count, setcount] =React.useState(0);
+  function addd(){
+    setcount(count+1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{count}</h1>
+      <button onClick={addd}>add</button>
     </div>
   );
 }
