2719081ab2ebf6a6f4b6560f7b4c2abbec1e8405
Mahmood787 <ali_khan87@live.com>
2020-06-11 13:39:52 +0500

    lines added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './my.css';
 
-function App(prop) {
-  let [count, setcount] =React.useState(0);
-  function addd(){
-    setcount(count+1);
-  }
-
+function App (prop){
   return (
-    <div className="App">
-      This app is created by {prop.name}
-      <h1>{count}</h1>
-      <button onClick={addd}>add</button>
+    <div>
+      <h2>Hello {prop.name}</h2>
+      <h4>Your fav fruits are</h4>
+      <ul>
+        <li>Apple</li>
+        <li>Oranges</li>
+        <li>Bananas</li>        
+      </ul>
+      <h4>your fav sports are</h4>
+      <ol>
+        <li>Cricket</li>
+        <li>Football</li>
+      </ol>
+
     </div>
-  );
+  )
 }
 
 export default App;
